9cfbcfaebdc0e142be79014019d36e4b9cd27951
agnieszka <agnieszka.hamal@gmail.com>
2022-04-16 18:12:20 +0200

    construct layout vol.2 + code

--- app/src/main/java/com/example/cookbook/ListActivity.java
+++ app/src/main/java/com/example/cookbook/ListActivity.java
@@ -1,7 +1,11 @@
 package com.example.cookbook;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ListActivity extends AppCompatActivity {
@@ -9,5 +13,17 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        getSupportActionBar().setTitle("Product list");
+        ImageView addNew=findViewById(R.id.addNewCategoryImageView);
+        addNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddCategoryDialog();            }
+        });
+    }
+    private void showAddCategoryDialog(){
+        AlertDialog dialogBuilder = new AlertDialog.Builder(this).create();
+        View dialogView = getLayoutInflater().inflate(R.layout.add_category_layout,null);
+        EditText enterCategoryImput=dialogView.findViewById(R.id.enterCategoryImput);
     }
 }
